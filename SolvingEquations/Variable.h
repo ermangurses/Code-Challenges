bf2ee8ed41735cf384651dd8420f0e815a1f1374
ermangurses <egurses@email.arizona.edu>
2018-04-09 16:13:03 -0700

    adding prototypes get set functions

--- SolvingEquations/Variable.h
+++ SolvingEquations/Variable.h
@@ -7,27 +7,36 @@
 #ifndef VARIABLE_H
 #define VARIABLE_H
 
-#include <string>
+#include <cctype>
 #include <cstdlib>
+#include <string>
 #include <iostream>
 #include <fstream>
 #include <sstream>
-#include <set>
+#include <unordered_set>
+
 class Variable{
 
   public:
     Variable();
+    Variable(std::string & line);
     ~Variable();
     void getLine(std::string  & line);
+
   private:
-    std::string variable;
-    std::set<std::string> dependencies;
+    std::string variable_name;
+    std::unordered_set<std::string> dependencies;
     int the_number_of_dependencies;
-    int sum_of_unsigned_integers;
-    void setVariableName();
-    void setDependencies();
-    void setValue();
+    unsigned int the_sum_of_unsigned_integers;
+    bool isVariableNameSet;
+    void setVariableName(std::string & variable_name);
+    void setDependencies(std::string & dependency_name);
     void parseLine(std::string & line);
     void sumValues(unsigned int value);
+    std::string getVariableName();
+    std::unordered_set<std::string> getDependencies();
+    unsinged int getTheNumberOfDependencies();
+    unsinged int getTheSumOfInsignedIntegers();
+    
 };
 #endif
