3dc7df90666f8f1ce99a59514c687921c11bafc2
ermangurses <egurses@email.arizona.edu>
2018-04-27 11:51:20 -0700

    Header file is formatted

--- SolvingEquations/Variable.h
+++ SolvingEquations/Variable.h
@@ -22,31 +22,32 @@ class Variable{
   public:
     Variable();
     Variable(std::string & line);
-   ~Variable();
-                               void getLine(std::string  & line);
-                      std::string & getVariableName();
-                               bool getDependency(std::string & dependency);
-                               bool removeDependency(std::string & dependency_name);
-                               void printDependencies();
-                       unsigned int getTheNumberOfDependencies();
-                               bool isDependencySetEmpty();
-                       unsigned int getTotalValue();
+    ~Variable();
+   
+    // functions
+    void getLine(std::string  & line);
+    std::string & getVariableName();
+    bool getDependency(std::string & dependency);
+    bool removeDependency(std::string & dependency_name);
+    bool printDependencies();
+    unsigned int getTheNumberOfDependencies();
+    bool isDependencySetEmpty();
+    unsigned int getTotalValue();
+    void addValue(unsigned int value);
   private:
-                                  std::string line; 
-                                  std::string variable_name;
-              std::unordered_set<std::string> dependencies;
-                                 unsigned int sum_of_unsigned_integers;
-                                         bool isVariableNameSet;
+    // variables
+    std::string line; 
+    std::string variable_name;
+    std::unordered_set<std::string> dependencies;
+    unsigned int sum_of_unsigned_integers;
+    bool isVariableNameSet;
     std::unordered_set<std::string>::iterator iter;
-                      std::queue<std::string> tokens;
-  
-
+    std::queue<std::string> tokens;
+    // functions
     void parseTokens();
     void tokenizer(std::string & line);
     bool getToken(std::string & token);
     void setVariableName(std::string & variable_name);
     void setDependency(std::string & dependency_name);
-    void addValue(unsigned int value);
-   
 };
 #endif // VARIABLE_H
