b57243ca07f1753c66930cd8beb70431c21871c6
ermangurses <egurses@email.arizona.edu>
2018-11-18 13:10:16 -0700

    One test case is added for the new implementation

--- E164Format/E164FormatTest.java
+++ E164Format/E164FormatTest.java
@@ -1,17 +1,19 @@
-//**************************************************
-//  
-//    E164FormatTest Driver Class
-//
-//**************************************************
+/** 
+ * E164FormatTest is the driver class that tests E164Format implementation
+ * @author Erman Gurses
+ * @version 1.0
+ * @since 2018-11-18
+ */
 
+public class E164FormatTest {
 
-public class E164FormatTest{
-
-    public static void main( String[] args ){
-
-        E164Format format = new E164Format();
-        format.setNumber("+1223222333");
-        format.validateNumber();
-
+    public static void main(String[] args) {
+        
+        E164Format formatNumber = new E164Format();
+        
+        formatNumber.setNumber("216562255");
+        System.out.println(formatNumber.convertNumber());
+       
     }
 }
+
