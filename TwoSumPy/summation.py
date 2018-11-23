25c9380ea1d51a448cf250b9186813d8ffbd1202
ermangurses <egurses@email.arizona.edu>
2018-11-22 20:52:45 -0700

    Adding summation class

---
+++ TwoSumPy/summation.py
@@ -0,0 +1,16 @@
+class Summation:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        d = {}
+        for i in range (len(nums)):
+            if nums[i] not in d:
+                d[nums[i]] = i
+            
+            if target - nums[i] in d:
+                if d[target - nums[i]] != i:
+                    return [d[target - nums[i]],i]
+        return[-1,-1]
