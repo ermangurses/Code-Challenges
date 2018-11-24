c297bd4659c969fcc165a65e8a11a7d62eb3fa56
ermangurses <egurses@email.arizona.edu>
2018-11-24 00:04:19 -0700

    Adding longest palindrome finder using center of the words

--- MaxPalindromePy/longest_palindrome.py
+++ MaxPalindromePy/longest_palindrome.py
@@ -41,3 +41,60 @@ class Palindrome:
                   
         palidromeString = s[isaved:jsaved+1]
         return palidromeString
+
+    def longestPalindromeSmart(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        savedRight = 0
+        savedLeft = 0
+        maximum = 0
+        if len(s) == 0:
+          return ""
+        if len(s) == 1:
+          return s[0]
+        if len(s) == 2 and s[0] == s[1]:
+          return s
+        if s[0] == s[1]:
+          maximum  = 1 - 0;
+          savedRight = 1
+          savedLeft = 0
+        if s[len(s)-2] == s[len(s)-1]:
+          maximum  = len(s)-1 - (len(s)-2)
+          savedRight = len(s)-1
+          savedLeft = len(s)-2          
+        for i in range (1, len(s) - 1):
+          #
+          # Odd Elements
+          #
+          left = i
+          right = i
+          while left > 0 and right < len(s) - 1 and s[left] == s [right]:
+            left = left - 1
+            right = right + 1
+            if s[left] == s [right]:
+              if right - left >  maximum:
+                maximum = right - left
+                savedRight = right
+                savedLeft = left
+          #
+          # Even Elements
+          #
+          left = i
+          right = i + 1
+          if s[left] == s[right]:
+            if right - left > maximum:
+              maximum = right - left
+              savedRight = right
+              savedLeft = left
+            while left > 0 and right < len(s) -1 and s[left] == s [right]:
+              left = left - 1
+              right = right + 1
+              if s[left] == s[right]:
+                if right - left > maximum:
+                  maximum = right - left
+                  savedRight = right
+                  savedLeft = left
+        return s[savedLeft:savedRight+1]    
+            
